3bf8b57d6cdab6c7d26c71b62bfccad8e90363b2
Doug Friedman <sillyville+github@gmail.com>
2016-04-24 01:40:49 -0400

    Added regression simulation module and cleaned up the UI

--- server.R
+++ server.R
@@ -2,6 +2,8 @@ library(SDMTools)
 library(DT)
 library(highcharter)
 library(boot)
+library(RcppArmadillo)
+library(dplyr, warn.conflicts=F)
 source("std_error_helpers.R")
 
 shinyServer(function(input, output, session) {
@@ -26,6 +28,28 @@ shinyServer(function(input, output, session) {
 	
 	return(clean_data)
   })
+  
+  
+  clean_data_lm = reactive({
+    req(input$datafile_lm); req(input$x_col); req(input$xr_col); req(input$y_col); 
+	datafile = input$datafile_lm
+
+	validate(
+	  need(substr(datafile$name, nchar(datafile$name)-3, nchar(datafile$name)) == ".csv", "Sorry, but the uploaded file could not be read. Please try using the template csv file.")
+	)
+
+	clean_data = read.csv(datafile$datapath, header=input$header, skip=input$skip_val)
+	clean_data = na.omit(clean_data[,c(input$x_col, input$xr_col, input$y_col)])
+	
+	validate(
+	  need(class(clean_data[,1]) %in% c("numeric", "integer"), "Sorry, but the X data detected contained non-numeric information. Please try using the toggles on the left to focus on the numeric data."),
+	  need(class(clean_data[,2]) %in% c("numeric", "integer"), "Sorry, but the X range data detected contained non-numeric information. Please try using the toggles on the left to focus on the numeric data."),
+	  need(class(clean_data[,3]) %in% c("numeric", "integer"), "Sorry, but the Y data detected contained non-numeric information. Please try using the toggles on the left to focus on the numeric data.")
+	)
+	
+	return(reg_sim(clean_data, input$nsim, input$force_origin))
+	#return(clean_data)
+  })  
   
   output$table_stats = DT::renderDataTable({
     req(input$stderr_calc)
@@ -54,16 +78,111 @@ shinyServer(function(input, output, session) {
 						"Variance",
 						"Standard Error")
 	
-	datatable(dt.df, options = list(dom = 't'))   
+	datatable(dt.df, options = list(dom = 't'), list(list(className = 'dt-center', targets = c(1:4))))   
 	
   })
-  
+
+  output$table_reg = DT::renderDataTable({
+
+    data = clean_data_lm()$res_sum
+	data = data %>% t %>% data.frame
+	data[,2] = paste0("(", data[,2], ", ", data[,3], ")")
+	data[,3] = NULL
+	names(data) = c("Mean", "95% CI")
+	rownames(data) = c("Slope", "Intercept", "R-Squared", "Root Mean Square Error", "Mean Square Weighted Deviation")
+
+    datatable(data, options = list(dom = 't', columnDefs = list(list(className = 'dt-center', targets = c(1:2)))))
+  })
+    
   
   output$dist_chart = renderHighchart({
 	data = clean_data()
 	
     hchart(data[,1], color="#173656", name = "Data")
 	  
+  })
+  
+  reg_sim = function(data, nsim, force_origin){
+    names(data) = c("x","x_range","y")
+    sim_data = matrix(nrow=nsim, ncol=length(data$x))
+
+	set.seed(7) # KEEPS IT RANDOM BUT REPRODUCIBLE
+	for(i in seq_along(data$x)){
+	  sim_data[,i] = runif(nsim, data[i,1] - data[i,2], data[i,1] + data[i,2])
+	}
+	
+	get_flm = function(x,y, origin=TRUE){  
+      this.lm = fastLm(cbind(x,!origin), y) 
+	  return(c(unname(coef(this.lm)), this.lm$fitted))
+	}
+	
+	# Extracted R2 and RMSE functions directly from caret library because of shinyapps.io limit
+	R2 = function (pred, obs, formula = "corr", na.rm = FALSE) 
+		{
+			n <- sum(complete.cases(pred))
+			switch(formula, corr = cor(obs, pred, use = ifelse(na.rm, 
+				"complete.obs", "everything"))^2, traditional = 1 - (sum((obs - 
+				pred)^2, na.rm = na.rm)/((n - 1) * var(obs, na.rm = na.rm))))
+		}
+	RMSE = function (pred, obs, na.rm = FALSE) { sqrt(mean((pred - obs)^2, na.rm = na.rm)) }
+    MSWD = function(pred, obs){ sum((obs - pred)^2 / obs)/(length(obs)-1) }
+	
+	sim_sum = function(simmed){ return(round(c(mean(simmed), quantile(simmed, c(0.025, .975), names=F)),3)) }
+	
+	sim.res = apply(sim_data, 1, get_flm, y=data$y, force_origin) 
+
+	coefs.sim = sim.res[c(1:2),] %>% apply(1, sim_sum)
+	fits = sim.res[-c(1:2),]
+
+	fit.rsq = apply(fits, 2, R2, obs=data$y)
+    fit.rmse = apply(fits, 2, RMSE, obs=data$y)	
+	fit.mswd = apply(fits, 2, MSWD, obs=data$y)
+
+	# Get three number summaries
+	slopes.sim = coefs.sim[,1]
+	ints.sim = coefs.sim[,2]
+	
+	rsq.sim = sim_sum(fit.rsq)
+	rmse.sim = sim_sum(fit.rmse)
+	mswd.sim = sim_sum(fit.mswd)
+
+	res = list(
+	  res_data = cbind(data, 
+	                   coef_mean= round(slopes.sim[1]*data$x + ints.sim[1],3),
+	                   coef_lci = round(slopes.sim[2]*data$x + ints.sim[2],3),
+					   coef_uci = round(slopes.sim[3]*data$x + ints.sim[3],3),
+					   fit_mean = round(apply(fits, 1, mean),3),
+					   fit_lci = round(apply(fits, 1, quantile, .025),3),
+					   fit_uci = round(apply(fits, 1, quantile, .975),3)
+			     ),
+	  res_sum = data.frame(slopes.sim, ints.sim, rsq.sim, rmse.sim, mswd.sim)
+	)
+    return(res)	
+  }  
+  
+  output$dist_reg = renderHighchart({
+	req(input$ci_method)
+	dataset = clean_data_lm()
+	dataset = dataset$res_data
+
+	#print(dataset)
+
+	# Each dataframe has to be laundered through list.parse3 
+	# with the columns assigned to the JS parameters
+	# except for the arearange chart which takes list.parse2
+	hc_data = list.parse3(data.frame(x=dataset$x, y=dataset$y))
+	hc_data2 = list.parse3(data.frame(x=dataset$x, y=dataset$coef_mean))
+	
+	lower = if(input$ci_method==2){ dataset$fit_lci } else { dataset$coef_lci }
+	upper = if(input$ci_method==2){ dataset$fit_uci } else { dataset$coef_uci }
+	hc_data3 = data.frame(x=dataset$x, lower, upper) %>% arrange(x) %>% list.parse2
+
+
+	highchart() %>%
+	  hc_add_series(data=hc_data, type="scatter", name="Original Data", color="#000") %>%
+	  hc_add_series(data=hc_data2, type="line", name="Regression Line", marker=list(enabled=FALSE), color="#214e6c") %>%
+	  hc_add_series(data=hc_data3, type="arearange", name="Bootstrapped 95% CI", fillOpacity=0.3, zIndex=1, dashStyle = "ShortDashDot") %>%
+	  hc_plotOptions(animation=FALSE)
   })
   
   output$downloadTemplate = downloadHandler(
@@ -75,4 +194,13 @@ shinyServer(function(input, output, session) {
     }
   )
 
+  output$downloadTemplate_lm = downloadHandler(
+   filename = function() {
+      paste("Sample", "Template_Regression.csv", sep='')
+    },
+    content = function(file) {
+      write.csv(data.frame(x = faithful[1:10,1], x_range = iris[1:10,4], y=faithful[1:10,2]), file, row.names=F)
+    }
+  )
+  
 })
--- ui.R
+++ ui.R
@@ -2,84 +2,169 @@ library(DT)
 library(highcharter)
 
 shinyUI(fluidPage(
-  
-  titlePanel(div(id="title",
-             img(src = "whoi_logo.gif", height="75px"),
+     # CSS was being difficult so I manually appended it to the head
+     tags$head(tags$link(rel="stylesheet", type="text/css", href="whoi_style.css")),
+
+     div(style="padding: 1px 0px; width: '100%'",
+         titlePanel(div(id="title", style="font-size:25px",
+             img(src = "whoi_logo.gif", height="55px"),
 			 "Adam's WHOI Application"),
 			 windowTitle = "Adam's App"
-			),
+			)
+       ),
+  navbarPage("Modules",
+      
+	  tabPanel("Standard Error", sidebarLayout(
+	  
+		sidebarPanel(
+		  helpText("This is an application designed to aid in performing regular statistical calculations for doctoral students at the Woods Hole Oceanographic Institute. You can upload a csv below and the application will automatically calculate several summary statistics and produce a plot of the data."),
 
-  sidebarLayout(
-  
-    sidebarPanel(
-	  helpText("This is an application designed to aid in performing regular statistical calculations for doctoral students at the Woods Hole Oceanographic Institute. You can upload a csv below and the application will automatically calculate several summary statistics and produce a plot of the data."),
-
+		  br(),
+		  helpText("If you are unsure what format your csv should take, you can download and edit a template csv using the link below."),
+		  downloadButton("downloadTemplate", "Download Template"),
+		  br(), br(),
+		  
+		  checkboxInput(
+			 inputId = "template_se",
+			 label = "My file matches the template.",
+			 value = TRUE
+		   ),
+		  br(),
+		  
+		  conditionalPanel(
+		    condition = "!input.template_se",
+			  h4("Please specify the following about the file..."),
+			  br(),
+			  
+			  numericInput(
+				inputId = "value_col",
+				label = "Which column contains the data?",
+				value = 1),
+				
+			  numericInput(
+				inputId = "weight_col",
+				label = "Which column contains the error?",
+				value = 2)	  
+		  ),
+		  radioButtons(
+			 inputId = "weight_calc",
+			 label = "How should the weights be calculated from the error?",
+			 choices = c("No Calculation"="None", "Inverse of the Square Root (1/SQRT)"="InvSqrt"),
+			 selected = "None"
+		  ),
+		  
+		  br(),
+		  
+		  checkboxInput(
+			 inputId = "header",
+			 label = "My file has column names.",
+			 value = TRUE
+		   ),
+		  
+		  conditionalPanel(
+			condition = "!input.header",
+			numericInput(
+			  inputId = "skip_val",
+			  label = "What is the first row with data?",
+			  value = 0)	
+		  ),
+		 
+		  
+		   
+		  br(), 	  
+		  
+		  fileInput(
+			inputId = "datafile", 
+			label = "Choose file to upload"
+			)
+		),
+		
+		mainPanel(
+		  h3("Distribution Plot"), highchartOutput("dist_chart", height="350px"),
+		  h3("Summary Statistics"), DT::dataTableOutput("table_stats"), br(), br(),
+		  radioButtons(
+			 inputId = "stderr_calc",
+			 label = "Choose a method to calculate standard error for the weighted mean.",
+			 choices = c("Boostrap"="bootstrap", "Formulaic"="formula"),
+			 selected = "bootstrap",
+			 inline=TRUE
+		   ),
+		  helpText("The calculation choices for standard error calculation for the weighted mean are taken from this 1995 paper by Donald Gatz and Luther Smith:", a("http://www.cs.tufts.edu/~nr/cs257/archive/donald-gatz/weighted-standard-error.pdf", href="http://www.cs.tufts.edu/~nr/cs257/archive/donald-gatz/weighted-standard-error.pdf"))
+		),
+	  ),
+	  h6(a(img(src="github_logo.png", height="20px"), 
+		 "Source code available on Github", href="https://github.com/doug-friedman/adam_app"))
+	  
+  ),
+  tabPanel("Regression", sidebarLayout(  
+	sidebarPanel(
+	  helpText("This is an application designed to aid in performing regular statistical calculations for doctoral students at the Woods Hole Oceanographic Institute. You can upload a csv below and the application will automatically simulate regression for the associated point ranges."),
+	  
 	  br(),
 	  helpText("If you are unsure what format your csv should take, you can download and edit a template csv using the link below."),
-	  downloadButton("downloadTemplate", "Download Template"),
-	  br(),
+	  downloadButton("downloadTemplate_lm", "Download Template"),
+	  br(), br(),
 	  
-	  br(),
-	  h4("Please specify the following about the file..."),
-	  br(),
-	  
-	  numericInput(
-	    inputId = "value_col",
-        label = "Which column contains the data?",
-		value = 1),
-		
-	  numericInput(
-	    inputId = "weight_col",
-        label = "Which column contains the error?",
-		value = 2),		  
-	  
-	  radioButtons(
-	     inputId = "weight_calc",
-		 label = "How should the weights be calculated from the error?",
-		 choices = c("No Calculation"="None", "Inverse of the Square Root (1/SQRT)"="InvSqrt"),
-		 selected = "None"
-      ),
-	  
-	  br(),
-	  
-	  checkboxInput(
-	     inputId = "header",
-		 label = "My file has column names.",
+	 checkboxInput(
+		 inputId = "template_reg",
+		 label = "My file matches the template.",
 		 value = TRUE
-       ),
+		   ),
+	  br(),
 	  
 	  conditionalPanel(
-	    condition = "!input.header",
-	    numericInput(
-	      inputId = "skip_val",
-          label = "What is the first row with data?",
-		  value = 0)	
-	  ),
-	 
+	    condition = "!input.template_reg",
+		  h4("Please specify the following about the file..."),
+		  br(),
+		  
+		  numericInput(
+			inputId = "x_col",
+			label = "Which column contains the independent variable or X?",
+			value = 1),
+			
+		  numericInput(
+			inputId = "xr_col",
+			label = "Which column contains the RANGE OF the indepndent variable or X?",
+			value = 2),	
+			
+		  numericInput(
+			inputId = "y_col",
+			label = "Which column contains the dependent variable or Y?",
+			value = 3)
+		),
+	  numericInput(
+		inputId = "nsim",
+		label = "How many simulations should be performed?",
+		value = 1000),	
+
+	  checkboxInput(
+			 inputId = "force_origin",
+			 label = "Force the regression through the origin (0,0).",
+			 value = TRUE
+		   ), br(),	   
+      radioButtons(
+	     	 inputId = "ci_method",
+			 label = "Choose confidence interval to display.",
+			 choices = c("Regression Coefficients"=1, "Fitted Values"=2),
+			 selected = 1,
+			 inline=TRUE
+		   ),
+		   
+	  br(),
 	  
-	   
-	  br(), 	  
+	   fileInput(
+			inputId = "datafile_lm", 
+			label = "Choose file to upload"
+			)
+  
 	  
-      fileInput(
-	    inputId = "datafile", 
-	    label = "Choose file to upload"
-	    )
 	),
-	
-    mainPanel(
-	  h3("Distribution Plot"), highchartOutput("dist_chart", height="350px"),
-	  h3("Summary Statistics"), DT::dataTableOutput("table_stats"), br(), br(),
-	  radioButtons(
-	     inputId = "stderr_calc",
-		 label = "Choose a method to calculate standard error for the weighted mean.",
-		 choices = c("Boostrap"="bootstrap", "Formulaic"="formula"),
-		 selected = "bootstrap",
-		 inline=TRUE
-       ),
-	  helpText("The calculation choices for standard error calculation for the weighted mean are taken from this 1995 paper by Donald Gatz and Luther Smith:", a("http://www.cs.tufts.edu/~nr/cs257/archive/donald-gatz/weighted-standard-error.pdf", href="http://www.cs.tufts.edu/~nr/cs257/archive/donald-gatz/weighted-standard-error.pdf"))
-    ),
-  ),
-  h6(a(img(src="github_logo.png", height="20px"), 
-	 "Source code available on Github", href="https://github.com/doug-friedman/adam_app")),
-  includeCSS("whoi.css")
+	mainPanel(
+	  h3("Regression Plot"), highchartOutput("dist_reg"), br(),
+	  h3("Simulation Results"), DT::dataTableOutput("table_reg"), br(),
+	  helpText("*Note: R-Squared values CANNOT be used to compare no-intercept and intercept models.", br(), "See", a(href="https://stats.stackexchange.com/questions/26176/removal-of-statistically-significant-intercept-term-increases-r2-in-linear-mo", "https://stats.stackexchange.com/questions/26176/removal-of-statistically-significant-intercept-term-increases-r2-in-linear-mo"), "for details."),
+	  br(), br()
+	)
+	))
+  )
 ))
